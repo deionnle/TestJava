f34ee6191a2b4053a6449ff650c1f722e790504e
deionnle <render48@yandex.ru>
2024-05-14 01:32:27 +0300

    add TestJava.java

---
+++ TestJava.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class TestJava {
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Эта программа определяет является ли некоторое целое положительное число, вводимое в программу, степенью двойки.");
+        System.out.println("Введите число");
+        int N = keyboard.nextInt();
+        while(N % 2 == 0)
+            N /= 2;
+        if(N == 1)
+        System.out.println("Да");
+        else
+        System.out.println("Нет");
+    }
+}
